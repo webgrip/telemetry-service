4bef272cc094c0c7ddc15f0b026635bf65c4e1d0
Ryan Grippeling <R.Grippeling@hotmail.com>
2024-10-15 07:01:48 +0200

    Moving stuff

--- src/Infrastructure/Services/TracingProxy.php
+++ src/Infrastructure/Services/TracingProxy.php
@@ -11,6 +11,7 @@ class TracingProxy
 {
     private object $instance;
     private TelemetryServiceInterface $telemetryService;
+    private bool $traceAllMethods;
 
     /**
      * @param object $instance
@@ -20,6 +21,10 @@ class TracingProxy
     {
         $this->instance = $instance;
         $this->telemetryService = $telemetryService;
+
+        // Check if the class itself is marked as Traceable
+        $reflectionClass = new ReflectionClass($this->instance);
+        $this->traceAllMethods = !empty($reflectionClass->getAttributes(Traceable::class));
     }
 
     /**
@@ -31,39 +36,29 @@ class TracingProxy
      */
     public function __call(string $method, array $arguments)
     {
-        $reflectionClass = new ReflectionClass($this->instance);
         $reflectionMethod = new ReflectionMethod($this->instance, $method);
-
-        // Check if the class or method has the Traceable attribute
-        $traceableClass = !empty($reflectionClass->getAttributes(Traceable::class));
         $traceableMethod = !empty($reflectionMethod->getAttributes(Traceable::class));
 
-        if ($traceableClass || $traceableMethod) {
-            // Determine the operation name based on the attribute or the method name
-            $operationName = $traceableClass
-                ? $reflectionClass->getShortName()
-                : $method;
+        if ($this->traceAllMethods || $traceableMethod) {
+            $operationName = $traceableMethod ?
+                $method :
+                (new ReflectionClass($this->instance))->getName();
 
-            // Start a span
             $span = $this->telemetryService->tracer()
                 ->spanBuilder($operationName)
                 ->startSpan();
             $scope = $span->activate();
 
             try {
-                // Call the original method
                 return $reflectionMethod->invokeArgs($this->instance, $arguments);
             } catch (\Throwable $e) {
-                // Record the exception and set status to error
                 $this->telemetryService->registerException($e, $span);
                 throw $e;
             } finally {
-                // End the span and detach the scope
                 $scope->detach();
                 $span->end();
             }
         } else {
-            // If no Traceable attribute, invoke the method as usual
             return $reflectionMethod->invokeArgs($this->instance, $arguments);
         }
     }
